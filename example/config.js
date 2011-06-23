082bef85f872310ae076ac93a185bbdb7c08196a
Bradley Meck <bradley.meck@gmail.com>
2011-06-23 16:08:55 -0500

    better merging with express

--- example/config.js
+++ example/config.js
@@ -3,55 +3,55 @@
 var clip = require('../');
 var app = new clip();
 
-app.config('.haibuconf',{
-  switches: ['c', 'conf']
+app.config('.appconf',{
+  flags: ['c', 'conf']
 });
 app.use(function(req,res,next){
-  console.log('Using config file '+req.config.store.file);
+  res.info('Using config file '+req.config.store.file);
   next();
 });
 
 app.usage(function usage(req,res) {
-  console.log('')
-  console.log('app')
-  console.log('  example CLI app for configuration files.')
-  console.log('  Please refer to documentation of commands using `-h` or `--help`.')
-  console.log('')
-  console.log('commands')
-  console.log('  app config')
-  console.log('')
+  res.info('')
+  res.info('app')
+  res.info('  example CLI app for configuration files.')
+  res.info('  Please refer to documentation of commands using `-h` or `--help`.')
+  res.info('')
+  res.info('commands')
+  res.info('  app config')
+  res.info('')
 })
 
 app.usage('/config',function config(req,res) {
-  console.log('')
-  console.log('app config')
-  console.log('  Actions related to the app configuration file.')
-  console.log('')
-  console.log('notes')
-  console.log('  The configuration will be found recursively up the file system.')
-  console.log('  If no configuration file is found the HOME folder will be used.')
-  console.log('  A default configuration file will be created if none exist.')
-  console.log('')
-  console.log('commands')
-  console.log('  app config get' + ' <id>')
-  console.log('  app config set' + ' <id> <value>')
-  console.log('')
-  console.log('options')
-  console.log('  -c --conf [.appconf]     The file to use as our configuration')
-  console.log('')
+  res.info('')
+  res.info('app config')
+  res.info('  Actions related to the app configuration file.')
+  res.info('')
+  res.info('notes')
+  res.info('  The configuration will be found recursively up the file system.')
+  res.info('  If no configuration file is found the HOME folder will be used.')
+  res.info('  A default configuration file will be created if none exist.')
+  res.info('')
+  res.info('commands')
+  res.info('  app config get' + ' <id>')
+  res.info('  app config set' + ' <id> <value>')
+  res.info('')
+  res.info('options')
+  res.info('  -c --conf [.appconf]     The file to use as our configuration')
+  res.info('')
 })
 
 app.cli('/config/get/:id',function configGet(req,res) {
-  console.log(req.params.id + ' = ' + (''+req.config.get(req.params.id)));
+  res.info(req.params.id + ' = ' + (''+req.config.get(req.params.id)));
 });
 app.usage('/config/get',function configGetUsage(req,res) {
-  console.log('')
-  console.log('app config get' + ' <id>')
-  console.log('  Gets the value of a property in the app configuration')
-  console.log('  See `app config -h` for more details')
-  console.log('')
-  console.log('params')
-  console.log('  id - nconf compatible name of the property')
+  res.info('')
+  res.info('app config get' + ' <id>')
+  res.info('  Gets the value of a property in the app configuration')
+  res.info('  See `app config -h` for more details')
+  res.info('')
+  res.info('params')
+  res.info('  id - nconf compatible name of the property')
 });
 
 app.cli('/config/set/:id/:value',function configSet(req,res) {
@@ -59,14 +59,14 @@ app.cli('/config/set/:id/:value',function configSet(req,res) {
   req.config.save();
 });
 app.usage('/config/set',function configSetUsage(req,res) {
-  console.log('')
-  console.log('app config set' + ' <id> <value>')
-  console.log('  Sets the value of a property in the app configuration')
-  console.log('  See `app config -h` for more details')
-  console.log('')
-  console.log('params')
-  console.log('  id - nconf compatible name of the property')
-  console.log('  value - json compatible value of the property')
+  res.info('')
+  res.info('app config set' + ' <id> <value>')
+  res.info('  Sets the value of a property in the app configuration')
+  res.info('  See `app config -h` for more details')
+  res.info('')
+  res.info('params')
+  res.info('  id - nconf compatible name of the property')
+  res.info('  value - json compatible value of the property')
 });
 
 app.run();
